57228b15efa8907e766932ef0eb68236397f0413
sebastyan1746 <geyselhard@gmail.com>
2026-04-05 14:59:12 +0300

    refactor: remove deprecated API and context files
    - Deleted the `Api`, `Bot`, `Composer`, and `Context` classes along with their type definitions and implementations.
    - Cleaned up related files in the `dist` directory to streamline the codebase.
    - This change is part of an effort to simplify the library and remove unused components.

--- dist/core/network/api/modules/subscriptions/api.d.ts
+++
@@ -1,7 +0,0 @@
-import { BaseApi } from '../../base-api';
-import { FlattenReq } from '../types';
-import type { CreateSubscriptionDTO, GetUpdatesDTO } from './types';
-export declare class SubscriptionsApi extends BaseApi {
-    create: ({ ...body }: FlattenReq<CreateSubscriptionDTO>) => Promise<import("./types").CreateSubscriptionResponse>;
-    getUpdates: ({ ...query }: FlattenReq<GetUpdatesDTO>) => Promise<import("./types").GetUpdatesResponse>;
-}
